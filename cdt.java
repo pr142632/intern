82e5645b296c28c87c9672d21470d29aa5d36afd
pr142632 <109205436+pr142632@users.noreply.github.com>
2022-08-18 12:26:51 +0530

    Add files via upload

---
+++ cdt.java
@@ -0,0 +1,14 @@
+package datepr;
+import java.util.Date;
+class cdt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Date p=new Date();
+		  
+		System.out.print("today date and time "+p);
+			}
+
+	
+
+}
